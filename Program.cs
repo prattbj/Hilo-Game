d97afc49a753e18b6dacb956382d277586daecee
JoshJ-WDD <joh21036@byui.edu>
2022-05-11 11:17:26 -0600

    Fixed problems in main program

--- Program.cs
+++ Program.cs
@@ -2,27 +2,37 @@
 {
     class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            public bool isPlaying = true;
             Player player = new Player(300);  
             getInput();                   
         }
 
         private static void getInput()
         {
-            int card = Card.randomInt();
+            Card card = new Card();
+            int drawnCard = card.randomInt();
             Console.WriteLine("The card is:", card);
         }
 
-        // public static void gameLoop()
-        // {
-        //     while (isPlaying) 
-        //     {
-        //         doOutput();
-        //         getInput();
-        //         doUpdate();
-        //     }
-        // }
+        private static void doOutput()
+        {
+            
+        }
+
+        private static void doUpdate()
+        {
+
+        }
+        public static void gameLoop()
+        {
+            bool isPlaying = true;
+            while (isPlaying) 
+            {
+                doOutput();
+                getInput();
+                doUpdate();
+            }
+        }
     }
 }
