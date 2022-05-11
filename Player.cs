8d52297545c9d0d90d6d017bbbac894129366ff1
prattbj <104381242+prattbj@users.noreply.github.com>
2022-05-11 10:49:30 -0600

    includes isLoss method

--- Player.cs
+++ Player.cs
@@ -1,28 +1,43 @@
+//Hilo game player class
+//Holds the score, has the logic for if it is a loss
 namespace Hilo
 {
     class Player
     {
         private int score;  
+        //Construct the player
         public Player(int score)
         {
             this.score = score;
         }
 
+        //increases the score
         public void increaseScore() 
         {
-            score += 100;
+            this.score += 100;
         }
 
+        //decreases the score
         public void decreaseScore()
         {
-            score -= 75;
+            this.score -= 75;
         }
 
+        //returns the score
         public int getScore()
         {
             return this.score;
         }
 
+        //returns if it is a loss
+        public bool isLoss()
+        {
+            if (this.score <= 0)
+            {
+                return true;
+            } else return false;
+
+        }
 
     }
 }
