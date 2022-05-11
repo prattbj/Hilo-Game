dedfaa23a618a7a679e82d423519c2ccdf0acdb9
prattbj <104381242+prattbj@users.noreply.github.com>
2022-05-11 10:36:30 -0600

    fixed constructor

--- Player.cs
+++ Player.cs
@@ -2,20 +2,20 @@ namespace Hilo
 {
     class Player
     {
-        private int score;
-        public Person(int score)
+        private int score;  
+        public Player(int score)
         {
             this.score = score;
         }
 
         public void increaseScore() 
         {
-
+            score += 100;
         }
 
         public void decreaseScore()
         {
-
+            score -= 75;
         }
 
         public int getScore()
