a23fda209e09aa707f8b00bdd0eb9c8738c5e995
JoshJ-WDD <joh21036@byui.edu>
2022-05-14 14:11:02 -0600

    Final commit - every requirement fulfilled

--- Card.cs
+++ Card.cs
@@ -2,16 +2,20 @@ namespace Hilo
 {
     class Card
     {
+        // Initialize random object
         Random random = new Random();
 
+        // Determine whether the guess was correct or not
         public bool isCorrect( int except, bool high )
         {
+            // Get the next random number, then modify it in order to avoid duplicates
+            // (this method is still reasonably random)
             int result = random.Next( 1, 12 );
             if (result >= except) result += 1;
             Console.WriteLine($"Next card was: {result}");
-            return ((result > except) == high);
+            return ((result > except) == high); // voodoo magic?
         }
-
+        // Returns a random number between 1 and 13 (any card in a deck of cards)
         public int randomInt()
         {
             return random.Next(1, 13);
--- Program.cs
+++ Program.cs
@@ -4,38 +4,84 @@
     {
         static void Main(string[] args)
         {
-            Player player = new Player(300);  
             gameLoop();                   
         }
-
-        private static void getInput()
+        // Draws a card and returns the result
+        private static int drawCard(Card card)
         {
-            Card card = new Card();
-            int drawnCard = card.randomInt();
-            Console.WriteLine("The card is: ", card);
+            return card.randomInt();
+        }
+
+        // Ask the user for input, then return a boolean that is true if the user guesses higher
+        //  or false if the user guesses lower or another character
+        private static bool getInput()
+        {
             Console.Write("Higher or lower? [h/l]: ");
+
             string entry = Console.ReadLine();
-            int entryValue = int.Parse(entry);
+            // Return true for high or false for low guess
+            if (entry == "h") {
+                return true;
+            } else {
+                return false;
+            }
         }
 
-        private static void doOutput()
+        // Display the score to the player
+        private static void showScore(Player player)
         {
-            
+            Console.WriteLine($"Your score is: {player.getScore()}");
         }
 
-        private static void doUpdate()
+        // Ask the user if they want to play again, return a boolean based on the response
+        private static bool playAgain()
         {
-
+            Console.Write("Would you like to play again? (y/n): ");
+            string response = Console.ReadLine();
+            // Return true if they enter y or false if they enter anything else
+            if (response == "y") {
+                return true;
+            } else {
+                return false;
+            }
         }
+        // Main loop of the game
         public static void gameLoop()
         {
+            // Set game as being played
             bool isPlaying = true;
+            // Initialize a new player and card
+            Player player = new Player(300);
+            Card card = new Card();
+
             while (isPlaying) 
             {
-                doOutput();
-                getInput();
-                doUpdate();
+                // Draw a new card, store the result in cardNum
+                int cardNum = drawCard(card);
+                // Display the card that has been draw
+                Console.WriteLine($"\nThe card is: {cardNum}");
+
+                // Get the input from the user, higher or lower (represented as true or false)
+                bool guess = getInput();
+
+                // If the user guesses correctly, increment their score, otherwise, decrement their score
+                if (card.isCorrect(cardNum, guess)) {
+                    player.increaseScore();
+                } else {
+                    player.decreaseScore();
+                }
+                // Display the modified score to the user
+                showScore(player);
+                // If the player runs out of points, tell the user and change the loop condition to false
+                if (player.isLoss()) {
+                    Console.WriteLine("Game over. You ran out of points.");
+                    isPlaying = false;
+                } else {
+                    isPlaying = playAgain();
+                }
             }
+            // Once the game is over, tell the user good game
+            Console.WriteLine("Good game!");
         }
     }
 }
